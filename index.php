44148e040e4805fb1b0e413d13794dc5654f4370
Douglas Nascimento <douglas.dans@gmail.com>
2020-08-10 17:37:35 -0300

    4 cmt

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <title>Produtos - DD Livros</title>
@@ -7,16 +8,17 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 </head>
-<body>
 
+<body>
   <header class="justify-content-end">
     <div class="nav-container justify-content-end ">
-      <nav class="navbar navbar-expand-md navbar-white fixed-top p-0 justify-content-end bg-pers0 p-2">
+      <nav class="navbar navbar-expand-md navbar-white fixed-top p-0 justify-content-end bg-blur bg-pers0 p-2">
         <div class="container">
           <a class="nav-link" href="index.html"><img src="img/DD-Paralax.png" height="35px"></a>
         </div>
       </nav>
-      <nav class="sticky-top position-fixed bg-pers0 text-white p-0" style="width: 100%;margin-top: 51px;height: 40px;">
+      <nav class="sticky-top position-fixed bg-blur bg-pers0 text-white p-0"
+        style="width: 100%;margin-top: 51px;height: 40px;">
         <div class="container nav-item navbar text-left navbar-expand-md justify-content">
           <a class="nav-link" href="index.html">Home</a>
           <a class="nav-link" href="produtos.html">Produtos</a>
@@ -33,18 +35,154 @@
         DINOVO ISSA MIERDA
       </div>
     </div>
-    <div class="container">
-      yfty
+    <div class="container mb-4 mt-4">
+
+      <div class="row">
+        <div class="col">
+          <div class="text-left gta-gCard text-center p-1 rounded-sm">
+            <a href="pagina0.3.html" class=""><img src="img/Capa Antiga.png" width="140px" height="190px"
+                class="rounded-lg p-0 mx-auto" alt="..."></a>
+            <div class="ml-2">
+              <div class="font-weight-bold text-center">
+                GTA Etec 0.3<br>
+                <small style="font-size: 60%">Lançamento: Setembro/2019</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="text-left gta-gCard text-center p-1 rounded-sm">
+            <a href="pagina1.0.html" class=""><img src="img/capa1.0.png" width="140px" height="190px"
+                class="rounded-lg p-0 mx-auto" alt="..."></a>
+            <div class="ml-2">
+              <div class="font-weight-bold text-center">
+                GTA Etec 1.0<br>
+                <small style="font-size: 60%">Lançamento: 09 Janeiro de 2020</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="text-left gta-gCard text-center p-1 rounded-sm">
+            <a href="pagina1.3.html" class=""><img src="img/capa1.3.png" width="140px" height="190px"
+                class="rounded-lg p-0 mx-auto" alt="..."></a>
+            <div class="ml-2">
+              <div class="font-weight-bold text-center">
+                GTA Etec 1.3<br>
+                <small style="font-size: 60%">Lançamento: 26 Agosto de 2020</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="text-left gta-gCard text-center p-1 rounded-sm">
+            <a href="https://etecspgov-my.sharepoint.com/:f:/g/personal/douglas_sousa43_etec_sp_gov_br/En-ALtGgqtdNrzdDAUyLWQkBFQa9JqsBIAX51tiKqRILKA?e=UdF0qS"
+              class=""><img src="img/onl.png" width="140px" height="190px" class="rounded-lg p-0 mx-auto" alt="..."></a>
+            <div class="ml-2">
+              <div class="font-weight-bold text-center">
+                GTA Etec Código-Fonte<br>
+                <small style="font-size: 60%">Todas as imagens e vídeos usados o jogo</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="text-left gta-gCard text-center p-1 rounded-sm">
+            <a href="https://etecspgov-my.sharepoint.com/:u:/g/personal/douglas_sousa43_etec_sp_gov_br/EUHNu6UXYGNGlYAGg1Q-m3QB0scvynpkMpeu5txlclC8QA"
+              class=""><img src="img/onlie.png" width="140px" height="190px" class="rounded-lg p-0 mx-auto"
+                alt="..."></a>
+            <div class="ml-2">
+              <div class="font-weight-bold text-center">
+                GTA Etec 1.1 Online<br>
+                <small style="font-size: 60%">Lançamento: Abril de 2020</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
 
+    <section class=" flush-with-above p-5 text-right">
+      <div class="container">
+        <div class="row justify-content-around align-items-center">
+          <div class="col-12 col-md-5 order-md-2 mb-5 mb-md-0">
+            <span class="title-decorative">GTA Etec 0.5</span>
+            <h5 class="h1">GTA Etec: Visítas Técnicas com o CJ</h5>
+            <p class="lead text-justify">
+              Essa foi a primeira tentativa de criar uma expansão pro GTA Etec, não se sabe ao certo, mas essa versão
+              foi lançada durante a 0.4/0.5 do jogo normal (mais ou menos em setembro/outubro de 2019). Ela permitia que
+              o jogador viajasse com o CJ para todos os mapas do GTA de PS2, além dos mapas de Bully e Manhunt, batendo
+              o recorde de 13Gb de jogo.
+            </p>
+          </div>
+          <!--end of col-->
+          <div class="col-12 col-md-5 order-md-1">
+            <img src="https://preview.ibb.co/dOW2sQ/GTAUG.png" alt="" height="300px">
+            <br><small>*imagem ilustrativa, esta versão foi totalmente perdida</small>
+          </div>
+          <!--end of col-->
+        </div>
+        <!--end of row-->
+      </div>
+      <!--end of container-->
+    </section>
 
+    <section class="secon flush-with-above p-5 bg-pers">
+      <div class="container">
+        <div class="row justify-content-around align-items-center">
+          <div class="col-12 col-md-6 col-lg-5 text-center text-md-left section-intro">
+            <span class="title-decorative">GTA Etec 1.0</span>
+            <h3 class="h1">GTA Etec Education</h3>
+            <span class="lead text-justify">
+              Isso ninguém imaginava, nessa época, o jogo normal estava crescendo cada dia mais a quantidade de GB, com
+              isso foi prometido uma versão lite, só que foi cancelada e tranformada no GTA Etec Education, que sim,
+              isso que você está pensando, uso educacional, infelizmente não deu certo por causa da dificuldade de criar
+              missões rapidamente.
+            </span>
+          </div>
+          <div class="col-12 col-md-5 order-md-1">
+            <img
+              src="https://vignette.wikia.nocookie.net/dyomworld/images/6/61/PgPtP.png/revision/latest/scale-to-width-down/340?cb=20110827122357"
+              alt="" height="300px">
+            <br><small>*imagem ilustrativa, perdi tbm foi mals</small>
+          </div>
+        </div>
+      </div>
+    </section>
 
-
+    <center style="background: white; height: auto; padding: 0px; margin-bottom: 15px;">
+      <img src="img/Americanas-empresas_home_marcas_01_samsung.png">
+      <img src="img/Americanas-empresas_home_marcas_02_dell.png">
+      <img src="img/Americanas-empresas_home_marcas_03_acer.png">
+      <img src="img/Americanas-empresas_home_marcas_04_apple.png">
+      <img src="img/Americanas-empresas_home_marcas_05_motorola.png">
+      <img src="img/Americanas-empresas_home_marcas_06_LG.png">
+      <img src="img/Americanas-empresas_home_marcas_07_brastemp.png">
+      <img src="img/Americanas-empresas_home_marcas_11_lenovo.png">
+      <img src="img/Americanas-empresas_MARCAS_marcas_13_microsoft.png">
+      <img src="img/Americanas-empresas_MARCAS_marcas_14_intel.png">
+      <img src="img/Americanas-empresas_MARCAS_marcas_14_multilaser.png">
+    </center>
 
 
 
 
   </main>
 </body>
+    <footer class="text-white bg-dark">
+      <div class="p-3 text-left float-left">
+        <img src="img/DD-Rmk-W.png" height="30px">
+      </div>
+      <div class="text-right p-3">
+        <small>DD Livros é uma marca registrada Paralax Company © 2019</small>
+        <br>
+        <small>© 2019 Paralax Design Site 5 é uma marca Grupo Liga</small>
+      </div>
+      <img src="img/fBorder.png" width=100% height="2px">
+    </footer>
+
 </html>
