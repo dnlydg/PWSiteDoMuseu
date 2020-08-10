94057af1260358215416898388bfd7f33b495ada
Douglas Nascimento <douglas.dans@gmail.com>
2020-08-10 16:25:09 -0300

    3 cmt

--- index.php
+++ index.php
@@ -1,41 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Produtos - DD Livros</title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/istilu.css">
+  <link rel="stylesheet" href="css/style.css">
   <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 </head>
 <body>
 
   <header class="justify-content-end">
     <div class="nav-container justify-content-end ">
-      <nav class="navbar navbar-expand-md navbar-white fixed-top p-0 justify-content-end nav-color p-2">
+      <nav class="navbar navbar-expand-md navbar-white fixed-top p-0 justify-content-end bg-pers0 p-2">
         <div class="container">
           <a class="nav-link" href="index.html"><img src="img/DD-Paralax.png" height="35px"></a>
-
-          <ul class="navbar-nav mr-auto nav justify-content-end">
-            <li class="nav-item active">
-
-            </li>
-          </ul>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-            </li>
-            <li class="nav-item ">
-              <a class="nav-link carrinho" href="carrinho.html"><img src="img/icone-carrinho.png" style="height: 40px;">
-                <font class="mt-5">Meu Carrinho</font>
-              </a>
-            </li>
-
-          </ul>
         </div>
       </nav>
-
-      <nav class="sticky-top position-fixed bg-dark text-white p-0" style="width: 100%;margin-top: 56px;height: 40px;">
+      <nav class="sticky-top position-fixed bg-pers0 text-white p-0" style="width: 100%;margin-top: 51px;height: 40px;">
         <div class="container nav-item navbar text-left navbar-expand-md justify-content">
           <a class="nav-link" href="index.html">Home</a>
           <a class="nav-link" href="produtos.html">Produtos</a>
@@ -43,8 +24,27 @@
           <a class="nav-link" href="contato.html">Fale Conosco</a>
         </div>
       </nav>
-
     </div>
   </header>
+
+  <main>
+    <div id="home-img">
+      <div class="mx-auto text-center text-white M-5">
+        DINOVO ISSA MIERDA
+      </div>
+    </div>
+    <div class="container">
+      yfty
+    </div>
+
+
+
+
+
+
+
+
+
+  </main>
 </body>
 </html>
